59534b05e4163113a3252c0772d8dc4bc19c08d1
Charles Bernard <bernardcharles@gmail.com>
2020-07-17 13:05:12 +0800

    Add new unit tests

--- src/test/scala/com/databricks/labs/validation/ValidatorTestSuite.scala
+++ src/test/scala/com/databricks/labs/validation/ValidatorTestSuite.scala
@@ -1,10 +1,10 @@
 package com.databricks.labs.validation
 
 import com.databricks.labs.validation.utils.Severity
-import com.databricks.labs.validation.utils.Structures.{Bounds, MinMaxRuleDef}
+import com.databricks.labs.validation.utils.Structures.{Bounds, DateBounds, MinMaxRuleDef}
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.functions.{col, min}
-import org.apache.spark.sql.types.{ArrayType, BooleanType, DateType, DoubleType, IntegerType, LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.functions.{col, min, to_date, array_contains, lit}
+import org.apache.spark.sql.types.{ArrayType, BooleanType, DoubleType, IntegerType, LongType, StringType, StructField, StructType}
 
 class ValidatorTestSuite extends org.scalatest.FunSuite with SparkSessionFixture {
 
@@ -18,8 +18,7 @@ class ValidatorTestSuite extends org.scalatest.FunSuite with SparkSessionFixture
       StructField("validNumerics",ArrayType(DoubleType,true),true) ::
       StructField("bounds",ArrayType(DoubleType,false),true) ::
       StructField("blank",BooleanType,true) ::
-      StructField("dataType",StringType,true) ::
-      StructField("dateBounds",ArrayType(DateType,true),true) :: Nil
+      StructField("dateBounds",ArrayType(StringType,false),true) :: Nil
   )
 
   val reportSchema = StructType(
@@ -43,16 +42,16 @@ class ValidatorTestSuite extends org.scalatest.FunSuite with SparkSessionFixture
 
   test("The input dataframe should have no rule failures on MinMaxRule") {
     val expectedData = Seq(
-      Row("MinMax_Cost_Generated_max","bounds",Row(null,null,null,null,Array(0.0, 12.0),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Cost_Generated_min","bounds",Row(null,null,null,null,Array(0.0, 12.0),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Cost_manual_max","bounds",Row(null,null,null,null,Array(0.0, 12.0),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Cost_manual_min","bounds",Row(null,null,null,null,Array(0.0, 12.0),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Cost_max","bounds",Row(null,null,null,null,Array(0.0, 12.0),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Cost_min","bounds",Row(null,null,null,null,Array(0.0, 12.0),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Sku_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Sku_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal)
+      Row("MinMax_Cost_Generated_max","bounds",Row(null,null,null,null,Array(0.0, 12.0),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Cost_Generated_min","bounds",Row(null,null,null,null,Array(0.0, 12.0),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Cost_manual_max","bounds",Row(null,null,null,null,Array(0.0, 12.0),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Cost_manual_min","bounds",Row(null,null,null,null,Array(0.0, 12.0),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Cost_max","bounds",Row(null,null,null,null,Array(0.0, 12.0),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Cost_min","bounds",Row(null,null,null,null,Array(0.0, 12.0),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Sku_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Sku_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal)
       )
     val expectedDF = spark.createDataFrame(spark.sparkContext.parallelize(expectedData), reportSchema)
 
@@ -85,10 +84,10 @@ class ValidatorTestSuite extends org.scalatest.FunSuite with SparkSessionFixture
 
   test("The input rule should have 1 invalid count for MinMax_Scan_Price_Minus_Retail_Price_min and max for failing complex type.") {
     val expectedData = Seq(
-      Row("MinMax_Retail_Price_Minus_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),1L,true,Severity.fatal),
-      Row("MinMax_Retail_Price_Minus_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),1L,true,Severity.fatal),
-      Row("MinMax_Scan_Price_Minus_Retail_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Scan_Price_Minus_Retail_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal)
+      Row("MinMax_Retail_Price_Minus_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),1L,true,Severity.fatal),
+      Row("MinMax_Retail_Price_Minus_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),1L,true,Severity.fatal),
+      Row("MinMax_Scan_Price_Minus_Retail_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Scan_Price_Minus_Retail_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal)
     )
     val expectedDF = spark.createDataFrame(spark.sparkContext.parallelize(expectedData), reportSchema)
     val testDF = Seq(
@@ -112,8 +111,8 @@ class ValidatorTestSuite extends org.scalatest.FunSuite with SparkSessionFixture
 
   test("The input rule should have 3 invalid count for failing aggregate type.") {
     val expectedData = Seq(
-      Row("MinMax_Min_Retail_Price","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Min_Scan_Price","bounds",Row(null,null,null,null,Array(3.0, 29.99),null,null,null),1L,true,Severity.fatal)
+      Row("MinMax_Min_Retail_Price","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Min_Scan_Price","bounds",Row(null,null,null,null,Array(3.0, 29.99),null,null),1L,true,Severity.fatal)
     )
     val expectedDF = spark.createDataFrame(spark.sparkContext.parallelize(expectedData), reportSchema)
     val testDF = Seq(
@@ -138,12 +137,12 @@ class ValidatorTestSuite extends org.scalatest.FunSuite with SparkSessionFixture
 
   test("The input dataframe should have exactly 1 rule failure on MinMaxRule") {
     val expectedData = Seq(
-      Row("MinMax_Cost_max","bounds",Row(null,null,null,null,Array(0.0, 12.00),null,null,null),1L,true,Severity.fatal),
-      Row("MinMax_Cost_min","bounds",Row(null,null,null,null,Array(0.0, 12.00),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Sku_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row("MinMax_Sku_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal)
+      Row("MinMax_Cost_max","bounds",Row(null,null,null,null,Array(0.0, 12.00),null,null),1L,true,Severity.fatal),
+      Row("MinMax_Cost_min","bounds",Row(null,null,null,null,Array(0.0, 12.00),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Sku_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row("MinMax_Sku_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal)
     )
     val expectedDF = spark.createDataFrame(spark.sparkContext.parallelize(expectedData), reportSchema)
     val testDF = Seq(
@@ -189,14 +188,14 @@ class ValidatorTestSuite extends org.scalatest.FunSuite with SparkSessionFixture
 
   test("The group by columns are the correct group by clauses in the validation") {
     val expectedData = Seq(
-      Row(3,"MinMax_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row(6,"MinMax_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row(3,"MinMax_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row(6,"MinMax_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row(3,"MinMax_Sku_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row(6,"MinMax_Sku_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row(3,"MinMax_Sku_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row(6,"MinMax_Sku_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal)
+      Row(3,"MinMax_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row(6,"MinMax_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row(3,"MinMax_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row(6,"MinMax_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row(3,"MinMax_Sku_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row(6,"MinMax_Sku_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row(3,"MinMax_Sku_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row(6,"MinMax_Sku_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal)
     )
     val expectedDF = spark.createDataFrame(spark.sparkContext.parallelize(expectedData), groupReportSchema)
     // 2 groups so count of the rules should yield (2 minmax rules * 2 columns) * 2 groups in cost (8 rows)
@@ -227,14 +226,14 @@ class ValidatorTestSuite extends org.scalatest.FunSuite with SparkSessionFixture
 
   test("The group by columns are with rules failing the validation") {
     val expectedData = Seq(
-      Row(3,"MinMax_Sku_Price_max","bounds",Row(null,null,null,null,Array(0.0, 0.0),null,null,null),1L,true,Severity.fatal),
-      Row(6,"MinMax_Sku_Price_max","bounds",Row(null,null,null,null,Array(0.0, 0.0),null,null,null),1L,true,Severity.fatal),
-      Row(3,"MinMax_Sku_Price_min","bounds",Row(null,null,null,null,Array(0.0, 0.0),null,null,null),1L,true,Severity.fatal),
-      Row(6,"MinMax_Sku_Price_min","bounds",Row(null,null,null,null,Array(0.0, 0.0),null,null,null),1L,true,Severity.fatal),
-      Row(3,"MinMax_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row(6,"MinMax_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row(3,"MinMax_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal),
-      Row(6,"MinMax_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null,null),0L,false,Severity.fatal)
+      Row(3,"MinMax_Sku_Price_max","bounds",Row(null,null,null,null,Array(0.0, 0.0),null,null),1L,true,Severity.fatal),
+      Row(6,"MinMax_Sku_Price_max","bounds",Row(null,null,null,null,Array(0.0, 0.0),null,null),1L,true,Severity.fatal),
+      Row(3,"MinMax_Sku_Price_min","bounds",Row(null,null,null,null,Array(0.0, 0.0),null,null),1L,true,Severity.fatal),
+      Row(6,"MinMax_Sku_Price_min","bounds",Row(null,null,null,null,Array(0.0, 0.0),null,null),1L,true,Severity.fatal),
+      Row(3,"MinMax_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row(6,"MinMax_Scan_Price_max","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row(3,"MinMax_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal),
+      Row(6,"MinMax_Scan_Price_min","bounds",Row(null,null,null,null,Array(0.0, 29.99),null,null),0L,false,Severity.fatal)
     )
     val expectedDF = spark.createDataFrame(spark.sparkContext.parallelize(expectedData), groupReportSchema)
     // 2 groups so count of the rules should yield (2 minmax rules * 2 columns) * 2 groups in cost (8 rows)
@@ -272,8 +271,8 @@ class ValidatorTestSuite extends org.scalatest.FunSuite with SparkSessionFixture
     ).toDF("product_name", "scan_price", "cost", "id")
 
     val numericLovExpectedData = Seq(
-      Row("CheckIfCostIsInLOV","validNumerics",Row(null,null,null,Array(3.0,6.0,99.0),null,null,null,null),0L,false,Severity.fatal),
-      Row("CheckIfScanPriceIsInLOV","validNumerics",Row(null,null,null,Array(2.51,5.11,8.22),null,null,null,null),0L,false,Severity.fatal),
+      Row("CheckIfCostIsInLOV","validNumerics",Row(null,null,null,Array(3.0,6.0,99.0),null,null,null),0L,false,Severity.fatal),
+      Row("CheckIfScanPriceIsInLOV","validNumerics",Row(null,null,null,Array(2.51,5.11,8.22),null,null,null),0L,false,Severity.fatal),
       Row("CheckIfIdIsInLOV","validNumerics",Row(null,null,null,Array(111111111111111.0,211111111111111.0,311111111111111.0),null,null,null,null),0L,false,Severity.fatal)
     )
     val numericLovExpectedDF = spark.createDataFrame(spark.sparkContext.parallelize(numericLovExpectedData), reportSchema)
@@ -299,7 +298,7 @@ class ValidatorTestSuite extends org.scalatest.FunSuite with SparkSessionFixture
     // Generate the array of Rules from the minmax generator
 
     val stringLovExpectedData = Seq(
-      Row("CheckIfProductNameInLOV","validStrings",Row(null,Array("food_a", "food_b", "food_c"),null,null,null,null,null,null),0L,false,Severity.fatal)
+      Row("CheckIfProductNameInLOV","validStrings",Row(null,Array("food_a", "food_b", "food_c"),null,null,null,null,null),0L,false,Severity.fatal)
     )
     val stringLovExpectedDF = spark.createDataFrame(spark.sparkContext.parallelize(stringLovExpectedData), reportSchema)
     val stringRuleSet = RuleSet(testDF)
@@ -320,8 +319,8 @@ class ValidatorTestSuite extends org.scalatest.FunSuite with SparkSessionFixture
     ).toDF("product_name", "scan_price", "cost", "id")
 
     val blankValidationExpectedData = Seq(
-      Row("CheckIfIdIsBlank","blank",Row(null,null,null,null,null,true,null,null),1L,false,"WARN"),
-      Row("CheckIfCostIsNotBlank","blank",Row(null,null,null,null,null,false,null,null),1L,false,"WARN")
+      Row("CheckIfIdIsBlank","blank",Row(null,null,null,null,null,true,null),1L,false,"WARN"),
+      Row("CheckIfCostIsNotBlank","blank",Row(null,null,null,null,null,false,null),1L,false,"WARN")
     )
 
     val blankValidationExpectedDF = spark.createDataFrame(spark.sparkContext.parallelize(blankValidationExpectedData), reportSchema)
@@ -340,7 +339,62 @@ class ValidatorTestSuite extends org.scalatest.FunSuite with SparkSessionFixture
     assert(blankValidated.except(blankValidationExpectedDF).count() == 0, "Expected df is not equal to the returned rules report.")
     assert(blankValidated.filter(blankValidated("Invalid_Count") > 0).count() == 2)
     assert(blankValidated.filter(blankValidated("Failed") === true).count() == 0)
-
   }
 
+  test("Validate that a date value is comprised within the given date bounds") {
+    val tmpTestDF = Seq(
+      ("1234", "2019-08-12"),
+      ("1235", "2000-03-23")
+    ).toDF("invoice_id", "date_received")
+    val testDF = tmpTestDF.withColumn("date_received", to_date($"date_received"))
+
+    val dateBoundsValidationExpectedData = Seq(
+      Row("CheckIfDateIsValid","dateBounds",Row(null,null,null,null,null,null,Array("2019-01-01","2019-12-31")),1L,true,"FATAL")
+    )
+
+    val dateBoundsValidationExpectedDF = spark.createDataFrame(spark.sparkContext.parallelize(dateBoundsValidationExpectedData), reportSchema)
+
+    val dateBoundsRules = Array(
+      Rule("CheckIfDateIsValid", col("date_received"), DateBounds("2019-01-01", "2019-12-31"), Severity.fatal)
+    )
+
+    val dateBoundsRuleSet = RuleSet(testDF)
+    dateBoundsRuleSet.add(dateBoundsRules)
+    val (dateBoundsValidated, dateBoundsPassed) = dateBoundsRuleSet.validate()
+
+    assert(dateBoundsRules.map(_.ruleType == RuleType.ValidateDateBounds).reduce(_ && _), "Not every value is validate blank.")
+    assert(!dateBoundsPassed)
+    assert(dateBoundsValidated.except(dateBoundsValidationExpectedDF).count() == 0, "Expected df is not equal to the returned rules report.")
+    assert(dateBoundsValidated.filter(dateBoundsValidated("Invalid_Count") > 0).count() == 1)
+    assert(dateBoundsValidated.filter(dateBoundsValidated("Failed") === true).count() == 1)
+  }
+
+  test("Validate value against adhoc validation") {
+    val testDF = Seq(
+      ("name_convention.123", "red"),
+      ("bad_name_convention.suffix", "blue")
+    ).toDF("name", "color")
+
+    val adhocValidationExpectedData = Seq(
+      Row("CheckIfNameMatchesConvention","adhoc",Row(null,null,"name RLIKE ^\\w+_\\w+.\\d+$",null,null,null,null),1L,true,"FATAL"),
+      Row("CheckIfColorIsValid","adhoc",Row(null,null,"array_contains([yellow,green,red], color)",null,null,null,null),1L,true,"FATAL")
+    )
+
+    val adhocValidationExpectedDF = spark.createDataFrame(spark.sparkContext.parallelize(adhocValidationExpectedData), reportSchema)
+
+    val adhocRules = Array(
+      Rule("CheckIfNameMatchesConvention", col("name").rlike("^\\w+_\\w+.\\d+$"), Severity.fatal),
+      Rule("CheckIfColorIsValid", array_contains(lit(Array("yellow", "green", "red")), col("color")), Severity.fatal)
+    )
+
+    val adhocRuleSet = RuleSet(testDF)
+    adhocRuleSet.add(adhocRules)
+    val (adhocValidated, adhocPassed) = adhocRuleSet.validate()
+
+    assert(adhocRules.map(_.ruleType == RuleType.ValidateAdhoc).reduce(_ && _), "Not every value is validate blank.")
+    assert(!adhocPassed)
+    assert(adhocValidated.except(adhocValidationExpectedDF).count() == 0, "Expected df is not equal to the returned rules report.")
+    assert(adhocValidated.filter(adhocValidated("Invalid_Count") > 0).count() == 2)
+    assert(adhocValidated.filter(adhocValidated("Failed") === true).count() == 2)
+  }
 }
